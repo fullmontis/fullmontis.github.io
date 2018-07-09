bd88b88327020c1ae8ab59a56ac33a7c18029764
Andrea <andrea>
2018-07-09 11:14:52 +0200

    Added contact

---
+++ test.js
@@ -0,0 +1,17 @@
+function run() {
+    var r = "hotmail";
+    var f = "tis";
+    var d = "enter";
+    var dd = "man";
+    var m = "@";
+    var e = "mon";
+    var w = ".co.uk";
+    var s = "yahoo";
+    var u = ".com";
+    var c = "full";
+    var q = "gmail";
+    var v = ".es";
+    var t = ".it";
+    var n = document.getElementById("em_slot");
+    n.innerHTML = c + e + f + m + q + u;
+}
